33d7cc87608afa84819d7ac035c364da073a2caf
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-04-12 20:09:21 -0400

    turn stub files into modules

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+
+// This no-op export should be deleted when you start using this file. It's
+// here to turn this file into a module so Typescript does not throw an error
+// on this file because of the--isolatedModules flag.
+export default null
