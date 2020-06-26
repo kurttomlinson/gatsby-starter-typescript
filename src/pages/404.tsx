8b84f88800e2fac6b5841d37bf06bf5bf2c6593c
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-06-26 00:00:51 -0400

    move components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Layout from "../components/llayout";
+import SEO from "../components/sseo";
 
 const NotFoundPage = () => (
   <Layout>
